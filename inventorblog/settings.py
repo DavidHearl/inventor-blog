1542eeb12a15751bfebcb03c36cd4cd18bf287fe
DavidHearl <david.hearl@outlook.com>
2022-05-09 20:56:41 +0000

    updated styling for login and register page

--- inventorblog/settings.py
+++ inventorblog/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = development
+DEBUG = True #development
 
 ALLOWED_HOSTS = ["inventor-blog.herokuapp.com", "localhost"]
 
